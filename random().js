7b60d448ab5c50bddd588d1ba4c3ee49651ec69b
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 12:39:49 +0500

    Create random().js

---
+++ random().js
@@ -0,0 +1,10 @@
+                  /*
+                         random() is used to generate random number.
+                            random() is used to generate random number between 0 and 1.
+
+
+
+
+                  */
+                           var a =  Math.floor(Math.random() * 10) + 1;
+                           document.write("<h1>Random number between 1 and 10 is: " + a + "</h1>");
