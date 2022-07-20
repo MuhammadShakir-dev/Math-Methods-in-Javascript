b5a783e887b529e46911dfc8a1b7fbdd5e9d8582
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 12:06:12 +0500

    Create ceil()&floor().js

---
+++ ceil()&floor().js
@@ -0,0 +1,18 @@
+ /*
+                        Math() Methods in javascript.
+
+                        whats is Math() in javascript?
+                        Math() is used to perform mathematical operation on number.
+
+                         var a = "100";
+
+                        ceil() is used to round the number up.
+                        floor() is used to round the number down.
+          */
+                        document.write("<h1>ceil()</h1>");
+                        var a = Math.ceil(3.142);
+                        document.write(a + "<br><br>");
+
+                        document.write("<h1>floor()</h1>");
+                        var b = Math.floor(3.142);
+                        document.write(b);
