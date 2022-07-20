779207b75baa97bbb350efd5886543c8186039a4
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 12:22:52 +0500

    Create min()max().js

---
+++ min()max().js
@@ -0,0 +1,14 @@
+          /*
+                 min() and max() is used to find the minimum and maximum value.
+                    min() is used to find the minimum value.
+                    max() is used to find the maximum value.
+
+
+
+
+          */
+           var a  = Math.min(prompt("Enter first value or number"), prompt("Enter second value or number"), prompt("Enter third value or number"));
+           document.write("<h1>Minimum value is: " + a + "</h1>");
+
+           var a = Math.max(prompt("Enter the first number"), prompt("Enter the second number"), prompt("Enter the third number"));
+          document.write("<h1>Maximum value is: " + a + "</h1>");
