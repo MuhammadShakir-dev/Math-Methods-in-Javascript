89d8da93c1582fb779d2bacbd49419edd1d3dba9
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 12:31:11 +0500

    Create sqrt()&cbrt().js

---
+++ sqrt()&cbrt().js
@@ -0,0 +1,14 @@
+                 /*
+                       sqrt() is used to find the square root of the number.
+                       cbrt() is used to find the cube root of the number.
+
+
+
+                 */
+                      var a = Math.sqrt(prompt("Enter a value for to find Square root"));
+                      document.write("<h1>Square root of is: " + a + "</h1>");                 
+ 
+
+                       var b = Math.cbrt(prompt("Enter a value for to find Cube root"));
+                       document.write("<h1> The cubic root is  :" +  b + "</h1>")
+
