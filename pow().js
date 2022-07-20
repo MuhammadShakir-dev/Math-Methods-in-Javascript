aef3f9c8fae5a59942e9e5e13ee4e354dc155332
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 12:34:27 +0500

    Create pow().js

---
+++ pow().js
@@ -0,0 +1,7 @@
+/*
+                            pow() is used to find the power of the number.
+
+                  */
+
+                  var a = Math.pow(prompt("Enter a base value"), prompt("Enter a power value"));
+                  document.write("<h1>The power of the number is: " + a + "</h1>");
