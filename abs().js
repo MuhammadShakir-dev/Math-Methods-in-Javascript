382bc7b8d9330a7a96ef7d4a5bca2729dd0b044a
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 12:43:48 +0500

    Create abs().js

---
+++ abs().js
@@ -0,0 +1,9 @@
+                  /*
+                        abs() is used to find the absolute value of the number.
+
+
+
+
+                  */
+                 var a = Math.abs(prompt("Enter a value"));
+                 document.write("<h1>The absolute value of the number is: " + a + "</h1>");
